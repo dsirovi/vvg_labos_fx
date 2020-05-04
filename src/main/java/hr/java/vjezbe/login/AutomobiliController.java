7a73a22093cc2493891c65e554dd1eaaf11a1e67
dsirovi <githubsifra1>
2020-05-04 21:16:59 +0200

    add scenes

---
+++ src/main/java/hr/java/vjezbe/login/AutomobiliController.java
@@ -0,0 +1,6 @@
+package hr.java.vjezbe.login;
+
+public class AutomobiliController {
+
+
+}
--- src/main/java/hr/java/vjezbe/login/Main.java
+++ src/main/java/hr/java/vjezbe/login/Main.java
@@ -8,14 +8,14 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
         primaryStage.setTitle("JavaFX application");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
