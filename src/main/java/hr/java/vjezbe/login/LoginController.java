55c550d537b0ac88521c9cc38603bee9afe5f4c6
dsirovi <domagoj.sirovina@hotmail.com>
2020-05-04 14:56:35 +0200

    add login check

--- src/main/java/hr/java/vjezbe/login/LoginController.java
+++ src/main/java/hr/java/vjezbe/login/LoginController.java
@@ -14,11 +14,35 @@ public class LoginController {
     private PasswordField passwordField;
 
     public void login(){
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Login Successful");
-        alert.setHeaderText(null);
-        alert.setContentText("Welcome " + usernameTextField.getText() + ", to application");
 
+        String username = usernameTextField.getText();
+        String password = passwordField.getText();
+
+        boolean error = false;
+        StringBuffer errorMessage = new StringBuffer();
+        if(username.isBlank()){
+            errorMessage.append("Please enter username");
+            error = true;
+        }
+        if (password.isEmpty()){
+            errorMessage.append("Invalid password!");
+            error = true;
+        }
+
+        Alert alert;
+        if (error){
+            alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Login Failed");
+            alert.setHeaderText(null);
+            alert.setContentText("Please enter correct username/password");
+
+        }else{
+            alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Login Successful");
+            alert.setHeaderText(null);
+            alert.setContentText("Welcome " + usernameTextField.getText() + ", to JavaFX application");
+
+        }
         alert.showAndWait();
     }
 }
